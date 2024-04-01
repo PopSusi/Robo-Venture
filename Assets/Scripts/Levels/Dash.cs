833829f39450e968577279efbe04f37e888a6cf8
Susi <braeden8.susi6@gmail.com>
2024-03-31 20:50:23 -0400

    Checkpoints set!

--- Assets/Scripts/Levels/Dash.cs
+++ Assets/Scripts/Levels/Dash.cs
@@ -7,7 +7,7 @@ public class Dash : RoboLevels
     // Start is called before the first frame update
     void Start()
     {
-        
+        RespawnPlayer();
     }
 
     // Update is called once per frame
