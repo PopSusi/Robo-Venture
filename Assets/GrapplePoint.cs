df9b5f3c161fd60b55a7583c4ccb4e110e49ebe3
Susi <braeden8.susi6@gmail.com>
2024-04-06 16:02:03 -0400

    Comments

--- Assets/GrapplePoint.cs
+++ Assets/GrapplePoint.cs
@@ -7,20 +7,31 @@ public class GrapplePoint : MonoBehaviour
 {
     public Image anchors;
     public bool active;
+    /// <summary>
+    /// Change size of anchor container, making it smaller/larger based on distance. 
+    /// If closest possible, activate little dot.
+    /// </summary>
+    /// <param name="size">Expands size of anchor container to this value (Between 205 - 275).</param>
     public void UpdateAnchors(float size)
     {
         if (active)
         {
             anchors.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
-            anchors.transform.GetChild(0).gameObject.SetActive(size == 205);
+            anchors.transform.GetChild(0).gameObject.SetActive(size == 205); //Activates little point
         }
     }
+    /// <summary>
+    /// Turn off Grapple Point UI.
+    /// </summary>
     public void Deactivate()
     {
         active = false;
         anchors.gameObject.SetActive(false);
         anchors.transform.GetChild(0).gameObject.SetActive(false);
     }
+    /// <summary>
+    /// Turn on Grapplepoint UI.
+    /// </summary>
     public void Activate()
     {
         anchors.gameObject.SetActive(true);
