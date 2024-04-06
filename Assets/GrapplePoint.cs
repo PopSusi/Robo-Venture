63e89aa1818129fb9ac6ddb1fb8dfa8b722b0024
Susi <braeden8.susi6@gmail.com>
2024-04-05 20:21:19 -0400

    UI Improvement

--- Assets/GrapplePoint.cs
+++ Assets/GrapplePoint.cs
@@ -9,12 +9,17 @@ public class GrapplePoint : MonoBehaviour
     public bool active;
     public void UpdateAnchors(float size)
     {
-        anchors.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
+        if (active)
+        {
+            anchors.GetComponent<RectTransform>().sizeDelta = new Vector2(size, size);
+            anchors.transform.GetChild(0).gameObject.SetActive(size == 205);
+        }
     }
     public void Deactivate()
     {
         active = false;
         anchors.gameObject.SetActive(false);
+        anchors.transform.GetChild(0).gameObject.SetActive(false);
     }
     public void Activate()
     {
